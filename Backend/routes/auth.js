86de5e2236fbc6f898748ccb5c35459cc3fb7624
divyansh1705 <divyanshdubey1713@gmail.com>
2024-11-10 22:08:52 +0530

    Adding authentication routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,1 +1,9 @@
-// This will contain authentication routes
+import express from "express"
+import { login, register } from "../controllers/authController.js";
+
+const router=express.Router();
+
+router.post('/register',register)
+router.post('/login',login)
+
+export default router
