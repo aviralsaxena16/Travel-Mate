0bc11898b9ed0184ec95a926b2293d2ea9ba2590
divyansh1705 <divyanshdubey1713@gmail.com>
2024-11-11 17:17:00 +0530

    Adding deleteTour controller

--- Backend/controllers/tourController.js
+++ Backend/controllers/tourController.js
@@ -24,3 +24,14 @@ export const updateTour = async (req, res) => {
         res.status(500).json({ success: false, message: "Failed to update, try again" });
     }
 };
+
+export const deleteTour = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await Tour.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Successfully Deleted" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Failed to delete, try again" });
+    }
+};
