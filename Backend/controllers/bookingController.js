8e11baddf5fe273287ef0bf71f9a13e17cc95408
divyansh1705 <divyanshdubey1713@gmail.com>
2024-11-14 00:44:14 +0530

    Added Get Booking Controller

--- Backend/controllers/bookingController.js
+++ Backend/controllers/bookingController.js
@@ -20,3 +20,29 @@ export const createBooking = async (req, res) => {
     }
 };
 
+export const getBooking = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const booking = await Booking.findById(id);
+        
+        if (!booking) {
+            return res.status(404).json({
+                success: false,
+                message: "Booking not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Booking retrieved successfully",
+            data: booking,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+};
+
