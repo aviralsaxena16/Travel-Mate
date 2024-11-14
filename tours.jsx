91ef9de2c662a6c81c9cda656fd5c0fc89a87270
kishor475 <kishorkoppikar@gmail.com>
2024-11-14 15:07:51 +0530

    front end

--- tours.jsx
+++ tours.jsx
@@ -10,6 +10,10 @@ import { useEffect, useState } from "react"
 const Tours =(){
     const [pageCount,setPageCount]=useState(0)
     const [page,setPage]=useState(0)
+    useEffect(()=>{
+        const pages = Math.ceil(5/4)
+        setPageCount(pages);
+    }, [page])
     return <>
         <CommonSection title={All Tours}
         <section>
@@ -24,11 +28,19 @@ const Tours =(){
                 <row>
                     tourData?.map(tour=><col lg='3' className='mb-4' key={tour.id}><TourCard tour={tour}/></col>)
                     <col lg='12'>
-                    
+                    <div className="pagination d-flex align-items-center justifu-cntent-center mt-4 gap-3">
+                        {[...Array(pagecount).keys()].map(number=>(
+                            <span key={number} onClick={()=>setPage(number)}
+                            className={page==number?"active__page":""}>
+                                {number+1}
+                            </span>
+                        ))}
+                    </div>
                     </col>
                 </row>
             </container>
         </section>
+        <NewsLetter />
     </>
 }
 export default Tours
